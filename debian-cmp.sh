6ad427dbdce9e31a5af81d8d40e0c381cd378082
Robert Starmer <rstarmer@gmail.com>
2017-01-24 00:16:15 -0800

    update debian launch scripts

--- debian-cmp.sh
+++ debian-cmp.sh
@@ -22,7 +22,7 @@ apt-get install \
 
 apt-get install apt-transport-https ca-certificates -y
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-deb https://apt.dockerproject.org/repo ubuntu-xenial main
+
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install docker-engine=1.12.6-0~ubuntu-xenial -y
--- debian-mn.sh
+++ debian-mn.sh
@@ -21,10 +21,9 @@ apt-get install \
     ca-certificates \
     bridge-utils -y
 
-#git clone https://github.com/JoeKuan/Network-Interfaces-Script nis
 apt-get install apt-transport-https ca-certificates -y
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-deb https://apt.dockerproject.org/repo ubuntu-xenial main
+
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install docker-engine=1.12.6-0~ubuntu-xenial -y
