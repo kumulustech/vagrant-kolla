b152873a3e0c21597e4e437c84cd51e6fa1845a8
Robert Starmer <rstarmer@gmail.com>
2017-01-24 17:01:49 -0800

    change default flavor

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 . ~/open.rc
 
-nova boot --flavor 3 --image cirros --nic net-id=`neutron net-list | awk '/ private / {print $2}'` test
+nova boot --flavor m1.small --image cirros --nic net-id=`neutron net-list | awk '/ private / {print $2}'` test
 floating_ip=`neutron floatingip-create public | awk '/ floating_ip_address / {print $4}'`
 nova floating-ip-associate test ${floating_ip}
 echo "Cirros instance should be available on ${floating_ip}"
