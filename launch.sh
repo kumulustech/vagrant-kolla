ce6ea8771653c2e47281de81fdca7e027981749c
Robert Starmer <rstarmer@gmail.com>
2017-01-24 23:39:05 -0800

    Update launch script to wait for VM to finish booting

--- launch.sh
+++ launch.sh
@@ -2,11 +2,13 @@
 
 . ~/open.rc
 
-nova boot --flavor m1.tiny --image cirros --nic net-id=`neutron net-list | awk '/ private / {print $2}'` test
+nova boot --flavor m1.tiny --image cirros --nic net-id=`neutron net-list | awk '/ private / {print $2}'` --poll cirros
+sleep 5
 floating_ip=`neutron floatingip-create public | awk '/ floating_ip_address / {print $4}'`
-nova floating-ip-associate test ${floating_ip}
+sleep 5
+nova floating-ip-associate cirros ${floating_ip}
+echo ""
 echo "Cirros instance should be available on ${floating_ip}"
 echo "try accessing via ssh from controller: cirros@${floating_ip}"
 echo "password is cubswin:)"
-
-
+echo ""
