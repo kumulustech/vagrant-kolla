f16beb392863db41919b8b033dc54e538239bf44
Robert Starmer <rstarmer@gmail.com>
2017-01-25 00:38:54 -0800

    update flavor to address ubuntu mininum disk size

--- add_flavor.sh
+++ add_flavor.sh
@@ -4,5 +4,5 @@
 
 openstack flavor create --public  --id  1 --ram 256 --disk 1 --vcpus 1 m1.tiny
 openstack flavor create --public  --id  3 --ram 512 --disk 1 --vcpus 1 m1.small
-openstack flavor create --public  --id  5 --ram 1024 --disk 2 --vcpus 2 m1.medium
+openstack flavor create --public  --id  5 --ram 1024 --disk 4 --vcpus 2 m1.medium
 
